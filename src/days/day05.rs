bce7b30952fae0b72c832fa8b14c92129ab3f0f9
Dallon Feldner <dallonfdev@gmail.com>
2023-12-05 08:59:44 -0600

    WIP day 5

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,5 +1,8 @@
 // Day 5: If You Give A Seed A Fertilizer
 
+use std::collections::HashMap;
+use std::str::FromStr;
+
 use crate::framework::Day;
 use crate::prelude::*;
 
@@ -19,9 +22,59 @@ impl Day for Day5 {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum Category {
+    Seed,
+    Soil,
+    Fertilizer,
+    Water,
+    Light,
+    Temperature,
+    Humidity,
+    Location,
+}
+
+impl FromStr for Category {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s {
+            "seed" => Ok(Category::Seed),
+            "soil" => Ok(Category::Soil),
+            "fertilizer" => Ok(Category::Fertilizer),
+            "water" => Ok(Category::Water),
+            "light" => Ok(Category::Light),
+            "temperature" => Ok(Category::Temperature),
+            "humidity" => Ok(Category::Humidity),
+            "location" => Ok(Category::Location),
+            _ => Err(anyhow!("Invalid category: {}", s)),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Almanac {
+    seeds: Vec<u32>,
+    maps: HashMap<(Category, Category), AlmanacMapList>,
+}
+
+#[derive(Debug)]
+struct AlmanacMapList(Vec<AlmanacMap>);
+
+#[derive(Debug)]
+struct AlmanacMap {
+    destination_range_start: u32,
+    source_range_start: u32,
+    range_length: usize,
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    fn example_input() -> &'static str {
+        include_str!("./day05_example_input.txt")
+    }
 
     #[test]
     fn test_part1() {
@@ -29,5 +82,12 @@ mod test {
             "Hello, world!".to_string(),
             super::Day5.part1().unwrap().unwrap()
         );
+    }
+
+    #[test]
+    fn test_parsing() {
+        let result = Almanac::from_str(example_input()).unwrap();
+        print!("{:#?}", result);
+        assert!(false);
     }
 }
