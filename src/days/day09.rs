fd6a1bc43b8a454a182b64e342fb052033f09b7f
Dallon Feldner <dallonfdev@gmail.com>
2023-12-09 03:40:07 -0600

    Day 9 Part Two

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -27,7 +27,12 @@ impl Day for Day9 {
     }
 
     fn part2(&self) -> Option<Result<String>> {
-        None
+        Some(try_block(move || {
+            let input = puzzle_input()?;
+            let results = input.par_iter().map(|it| extrapolate_backwards(it));
+            let sum: i32 = results.sum();
+            Ok(sum.to_string())
+        }))
     }
 }
 
@@ -50,6 +55,14 @@ fn extrapolate(input: &[i32]) -> i32 {
     input.last().copied().unwrap_or(0) + next_diff
 }
 
+fn extrapolate_backwards(input: &[i32]) -> i32 {
+    if input.iter().all(|&it| it == 0) {
+        return 0;
+    }
+    let next_diff = extrapolate_backwards(&get_difference_sequence(input));
+    input.first().copied().unwrap_or(0) - next_diff
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -60,6 +73,11 @@ mod test {
             super::Day9.part1().unwrap().unwrap(),
             "1637452029".to_string(),
         );
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(super::Day9.part2().unwrap().unwrap(), "0".to_string());
     }
 
     fn sample_input() -> Vec<Vec<i32>> {
@@ -93,6 +111,17 @@ mod test {
         let input = sample_input();
         let results = input.iter().map(|it| extrapolate(it)).collect_vec();
         let expected = vec![18, 28, 68];
+        assert_eq!(results, expected);
+    }
+
+    #[test]
+    fn test_extrapolate_backwards() {
+        let input = sample_input();
+        let results = input
+            .iter()
+            .map(|it| extrapolate_backwards(it))
+            .collect_vec();
+        let expected = vec![-3, 0, 5];
         assert_eq!(results, expected);
     }
 }
