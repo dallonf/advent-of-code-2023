22b9940bb62d22093366bf15733e417c86627c01
Dallon Feldner <dallon@sibipro.com>
2023-12-19 11:37:21 -0600

    Day 19 Part Two

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -2,6 +2,7 @@
 
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::ops::RangeInclusive;
 use std::str::FromStr;
 
 use regex::Regex;
@@ -29,7 +30,11 @@ impl Day for Day19 {
     }
 
     fn part2(&self) -> Option<Result<String>> {
-        None
+        Some(try_block(move || {
+            puzzle_input()?
+                .total_accepted_combinations()
+                .map(|it| it.to_string())
+        }))
     }
 }
 
@@ -55,13 +60,13 @@ impl RatingCategory {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Part {
-    x: u32,
-    m: u32,
-    a: u32,
-    s: u32,
+    x: u16,
+    m: u16,
+    a: u16,
+    s: u16,
 }
 impl Part {
-    fn get(&self, category: RatingCategory) -> u32 {
+    fn get(&self, category: RatingCategory) -> u16 {
         match category {
             RatingCategory::X => self.x,
             RatingCategory::M => self.m,
@@ -96,6 +101,52 @@ impl FromStr for Part {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct PartRange {
+    x: RangeInclusive<u16>,
+    m: RangeInclusive<u16>,
+    a: RangeInclusive<u16>,
+    s: RangeInclusive<u16>,
+}
+
+impl PartRange {
+    fn new() -> Self {
+        Self {
+            x: 1..=4000,
+            m: 1..=4000,
+            a: 1..=4000,
+            s: 1..=4000,
+        }
+    }
+
+    fn get(&self, category: RatingCategory) -> &RangeInclusive<u16> {
+        match category {
+            RatingCategory::X => &self.x,
+            RatingCategory::M => &self.m,
+            RatingCategory::A => &self.a,
+            RatingCategory::S => &self.s,
+        }
+    }
+
+    fn with_set(&self, category: RatingCategory, range: RangeInclusive<u16>) -> Self {
+        let mut new = self.clone();
+        match category {
+            RatingCategory::X => new.x = range,
+            RatingCategory::M => new.m = range,
+            RatingCategory::A => new.a = range,
+            RatingCategory::S => new.s = range,
+        }
+        new
+    }
+
+    fn combinations(&self) -> u64 {
+        (self.x.end() - self.x.start() + 1) as u64
+            * (self.m.end() - self.m.start() + 1) as u64
+            * (self.a.end() - self.a.start() + 1) as u64
+            * (self.s.end() - self.s.start() + 1) as u64
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Outcome {
     Workflow(String),
     Accept,
@@ -118,8 +169,74 @@ impl FromStr for Outcome {
 struct Rule {
     category: RatingCategory,
     comparison: Ordering,
-    value: u32,
+    value: u16,
     outcome: Outcome,
+}
+
+impl Rule {
+    fn follow(&self, part: &Part) -> Option<Outcome> {
+        let value = part.get(self.category);
+        if self.comparison == value.cmp(&self.value) {
+            Some(self.outcome.clone())
+        } else {
+            None
+        }
+    }
+
+    fn split_on_rule(&self, range: &PartRange) -> Result<RuleSplitResult> {
+        use RuleSplitResult::*;
+
+        let relevant_range = range.get(self.category);
+        let result = match self.comparison {
+            Ordering::Less => {
+                if *relevant_range.start() > self.value {
+                    NoMatch
+                } else if *relevant_range.end() < self.value {
+                    AllMatch(self.outcome.clone())
+                } else {
+                    let matched =
+                        range.with_set(self.category, *relevant_range.start()..=(self.value - 1));
+                    let unmatched =
+                        range.with_set(self.category, (self.value)..=*relevant_range.end());
+                    SomeMatch {
+                        matched,
+                        unmatched,
+                        outcome: self.outcome.clone(),
+                    }
+                }
+            }
+            Ordering::Greater => {
+                if *relevant_range.end() < self.value {
+                    NoMatch
+                } else if *relevant_range.start() > self.value {
+                    AllMatch(self.outcome.clone())
+                } else {
+                    let matched =
+                        range.with_set(self.category, (self.value + 1)..=*relevant_range.end());
+                    let unmatched =
+                        range.with_set(self.category, *relevant_range.start()..=(self.value));
+                    SomeMatch {
+                        matched,
+                        unmatched,
+                        outcome: self.outcome.clone(),
+                    }
+                }
+            }
+            Ordering::Equal => return Err(anyhow!("'=' rules not supported for range operations")),
+        };
+        Ok(result)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum RuleSplitResult {
+    NoMatch,
+    AllMatch(Outcome),
+    SomeMatch {
+        matched: PartRange,
+        unmatched: PartRange,
+        outcome: Outcome,
+    },
 }
 
 lazy_static! {
@@ -161,9 +278,8 @@ struct Workflow {
 impl Workflow {
     fn follow(&self, part: &Part) -> Outcome {
         for rule in &self.rules {
-            let value = part.get(rule.category);
-            if rule.comparison == value.cmp(&rule.value) {
-                return rule.outcome.clone();
+            if let Some(outcome) = rule.follow(part) {
+                return outcome;
             }
         }
         self.fallback.clone()
@@ -213,6 +329,52 @@ impl WorkflowSeries {
         }
         Ok(outcome)
     }
+
+    fn accepted_combinations(&self, range: PartRange, workflow_id: &str) -> Result<u64> {
+        let workflow = self
+            .workflows
+            .get(workflow_id)
+            .ok_or(anyhow!("Workflow not found: {}", workflow_id))?;
+        let mut remaining_range = range.clone();
+        let mut accepted = 0;
+        for rule in &workflow.rules {
+            match rule.split_on_rule(&remaining_range)? {
+                RuleSplitResult::NoMatch => {}
+                RuleSplitResult::AllMatch(outcome) => {
+                    return Ok(accepted
+                        + match outcome {
+                            Outcome::Accept => remaining_range.combinations(),
+                            Outcome::Reject => 0,
+                            Outcome::Workflow(workflow_id) => {
+                                self.accepted_combinations(remaining_range, &workflow_id)?
+                            }
+                        })
+                }
+                RuleSplitResult::SomeMatch {
+                    matched,
+                    unmatched,
+                    outcome,
+                } => {
+                    remaining_range = unmatched;
+                    accepted += match outcome {
+                        Outcome::Accept => matched.combinations(),
+                        Outcome::Reject => 0,
+                        Outcome::Workflow(workflow_id) => {
+                            self.accepted_combinations(matched, &workflow_id)?
+                        }
+                    }
+                }
+            }
+        }
+        accepted += match &workflow.fallback {
+            Outcome::Accept => remaining_range.combinations(),
+            Outcome::Workflow(workflow_id) => {
+                self.accepted_combinations(remaining_range, workflow_id)?
+            }
+            Outcome::Reject => 0,
+        };
+        Ok(accepted)
+    }
 }
 
 impl FromStr for WorkflowSeries {
@@ -250,6 +412,10 @@ impl Input {
             .map_ok(|(part, _)| part.rating())
             .sum::<Result<u64>>()
     }
+
+    fn total_accepted_combinations(&self) -> Result<u64> {
+        self.workflows.accepted_combinations(PartRange::new(), "in")
+    }
 }
 
 impl FromStr for Input {
@@ -273,9 +439,14 @@ mod test {
 
     #[test]
     fn test_part1() {
+        assert_eq!(super::Day19.part1().unwrap().unwrap(), "402185".to_string(),);
+    }
+
+    #[test]
+    fn test_part2() {
         assert_eq!(
-            super::Day19.part1().unwrap().unwrap(),
-            "402185".to_string(),
+            super::Day19.part2().unwrap().unwrap(),
+            "130291480568730".to_string(),
         );
     }
 
@@ -380,5 +551,14 @@ mod test {
     fn test_accepted_parts_rating() {
         let input = sample_input();
         assert_eq!(input.accepted_parts_rating().unwrap(), 19114);
+    }
+
+    #[test]
+    fn test_accepted_combinations() {
+        let input = sample_input();
+        assert_eq!(
+            input.total_accepted_combinations().unwrap(),
+            167409079868000
+        );
     }
 }
