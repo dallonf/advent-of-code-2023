0781b3b9f476b209821fb3339b215d72581d95f8
Dallon Feldner <dallonfdev@gmail.com>
2023-12-20 19:54:36 -0600

    Update main script to support arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::time::{Duration, Instant};
 
 use crate::framework::Day;
+use clap::Parser;
+use framework::prelude;
+use prelude::*;
 
+mod framework;
 mod days {
     pub mod day00;
     pub mod day01;
@@ -25,12 +29,9 @@ mod days {
     pub mod day19;
     pub mod day20;
 }
-mod framework;
 
-use framework::prelude;
-
-fn main() {
-    let days: Vec<Box<dyn Day>> = vec![
+lazy_static! {
+    static ref DAYS: Vec<Box<dyn Day + Sync>> = vec![
         Box::new(days::day01::Day1),
         Box::new(days::day02::Day2),
         Box::new(days::day03::Day3),
@@ -52,30 +53,54 @@ fn main() {
         Box::new(days::day19::Day19),
         Box::new(days::day20::Day20),
     ];
+}
 
-    for day in days.iter() {
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(short, long)]
+    day: Option<u8>,
+    #[arg(short, long)]
+    part: Option<u8>,
+    /// Run a custom script instead of the day's code. `day` is required if this option is used.
+    #[arg(short, long)]
+    script: Option<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for day in DAYS.iter() {
         if day.day_number() == 0 {
             println!("WARNING: Did you forget to change the day_number() for one of the days?");
         }
-        let day1_start = Instant::now();
-        if let Some(output) = day.part1() {
-            let day1_elapsed = day1_start.elapsed();
-            println!(
-                "Day {}, part 1 ({}): {}",
-                day.day_number(),
-                format_duration(&day1_elapsed),
-                output.unwrap_or_else(|err| err.to_string()),
-            );
+
+        if args.day == Some(day.day_number()) {
+            continue;
         }
-        let day2_start = Instant::now();
-        if let Some(output) = day.part2() {
-            let day2_elapsed = day2_start.elapsed();
-            println!(
-                "Day {}, part 2 ({}): {}",
-                day.day_number(),
-                format_duration(&day2_elapsed),
-                output.unwrap_or_else(|err| err.to_string()),
-            );
+
+        if args.part.is_none() || args.part == Some(1) {
+            let day1_start = Instant::now();
+            if let Some(output) = day.part1() {
+                let day1_elapsed = day1_start.elapsed();
+                println!(
+                    "Day {}, part 1 ({}): {}",
+                    day.day_number(),
+                    format_duration(&day1_elapsed),
+                    output.unwrap_or_else(|err| err.to_string()),
+                );
+            }
+        }
+        if args.part.is_none() || args.part == Some(2) {
+            let day2_start = Instant::now();
+            if let Some(output) = day.part2() {
+                let day2_elapsed = day2_start.elapsed();
+                println!(
+                    "Day {}, part 2 ({}): {}",
+                    day.day_number(),
+                    format_duration(&day2_elapsed),
+                    output.unwrap_or_else(|err| err.to_string()),
+                );
+            }
         }
     }
 }
