7ed20e719b97610e95fa615662ca3051e2ea88e9
Dallon Feldner <dallonfdev@gmail.com>
2023-12-23 22:11:01 -0600

    Day 22 Part One, incorrect

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,6 +1,6 @@
 // Day 22: Sand Slabs
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Write;
 use std::ops::RangeInclusive;
 use std::str::FromStr;
@@ -8,6 +8,11 @@ use std::str::FromStr;
 use crate::framework::grid::{GridShape, IntVector};
 use crate::framework::Day;
 use crate::prelude::*;
+
+fn puzzle_input() -> Result<World> {
+    let input = include_str!("./day22_input.txt");
+    input.parse()
+}
 
 pub struct Day22;
 
@@ -17,7 +22,11 @@ impl Day for Day22 {
     }
 
     fn part1(&self) -> Option<Result<String>> {
-        Some(try_block(move || "Hello, world!".to_string().pipe(Ok)))
+        Some(try_block(move || {
+            let mut world = puzzle_input()?;
+            world.apply_gravity();
+            Ok(world.find_non_load_bearing_bricks().to_string())
+        }))
     }
 
     fn part2(&self) -> Option<Result<String>> {
@@ -87,31 +96,84 @@ impl FromStr for Brick {
 
 struct World {
     /// Should always be sorted by lowest_z in ascending order.
-    bricks: Vec<Brick>,
+    bricks: Vec<(usize, Brick)>,
+    /// Key: brick ID, Value: IDs of bricks that this brick supports
+    brick_support: HashMap<usize, HashSet<usize>>,
+    /// Key: brick ID, Value: IDs of bricks that support this brick
+    supported_by: HashMap<usize, HashSet<usize>>,
 }
 
 impl World {
+    fn new(bricks: Vec<Brick>) -> Self {
+        Self {
+            bricks: bricks.into_iter().enumerate().collect(),
+            brick_support: HashMap::new(),
+            supported_by: HashMap::new(),
+        }
+    }
+
     fn sort(&mut self) {
-        self.bricks.sort_by_key(|brick| brick.lowest_z());
+        self.bricks.sort_by_key(|(_, brick)| brick.lowest_z());
     }
 
     fn apply_gravity(&mut self) {
-        let mut highest_grounded_point = HashMap::<IntVector, isize>::new();
-        for brick in self.bricks.iter_mut() {
+        #[derive(Debug, Clone, Copy)]
+        struct BrickCube {
+            id: usize,
+            position: IntVector3,
+        }
+        let mut highest_grounded_point = HashMap::<IntVector, BrickCube>::new();
+        for (id, brick) in self.bricks.iter_mut() {
             let brick_lowest_z = brick.lowest_z();
             let horizontal_coords = (brick.0.x..=brick.1.x)
                 .flat_map(|x| (brick.0.y..=brick.1.y).map(move |y| IntVector::new(x, y)))
                 .collect_vec();
-            let stop_z = horizontal_coords
-                .iter()
-                .map(|vec2d| highest_grounded_point.get(vec2d).copied().unwrap_or(0) + 1)
-                .max()
-                .expect("Brick must exist on the XY plane");
+            let (stop_z, bricks_underneath) = {
+                let bricks_underneath = horizontal_coords
+                    .iter()
+                    .filter_map(|vec2d| highest_grounded_point.get(vec2d))
+                    .copied()
+                    .collect_vec();
+                let stop_z = bricks_underneath
+                    .iter()
+                    .map(|cube| cube.position.z)
+                    .max()
+                    .unwrap_or(0)
+                    + 1;
+                let bricks_underneath = bricks_underneath
+                    .into_iter()
+                    .filter(|cube| cube.position.z == stop_z - 1)
+                    .collect_vec();
+                (stop_z, bricks_underneath)
+            };
+
             let z_diff = brick_lowest_z - stop_z;
             brick.0.z -= z_diff;
             brick.1.z -= z_diff;
             for coord in horizontal_coords {
-                highest_grounded_point.insert(coord, stop_z);
+                let cube_position = IntVector3 {
+                    x: coord.x,
+                    y: coord.y,
+                    z: stop_z,
+                };
+                highest_grounded_point.insert(
+                    coord,
+                    BrickCube {
+                        id: *id,
+                        position: cube_position,
+                    },
+                );
+            }
+
+            for brick_underneath in bricks_underneath {
+                self.brick_support
+                    .entry(brick_underneath.id)
+                    .or_default()
+                    .insert(*id);
+                self.supported_by
+                    .entry(*id)
+                    .or_default()
+                    .insert(brick_underneath.id);
             }
         }
 
@@ -119,17 +181,44 @@ impl World {
     }
 
     fn get_top_z(&self) -> Option<isize> {
-        self.bricks.iter().map(|brick| brick.highest_z()).max()
+        self.bricks.iter().map(|(_, brick)| brick.highest_z()).max()
+    }
+
+    fn is_load_bearing(&self, id: usize) -> Option<bool> {
+        let supported_bricks = match { self.brick_support.get(&id) } {
+            Some(supported_bricks) => supported_bricks,
+            None => return Some(false),
+        };
+        if supported_bricks.is_empty() {
+            return Some(true);
+        }
+        let is_only_support = supported_bricks.iter().any(|supported_brick_id| {
+            let supported_by = self.supported_by.get(supported_brick_id).unwrap();
+            supported_by.len() == 1
+        });
+        Some(is_only_support)
+    }
+
+    fn find_non_load_bearing_bricks(&self) -> usize {
+        self.bricks
+            .iter()
+            .filter(|(id, _)| {
+                !self
+                    .is_load_bearing(*id)
+                    .expect("is_load_bearing should always return Some")
+            })
+            .count()
     }
 
     fn debug_xz_plane(&self) -> String {
+        let visible_ids = ('A'..'Z').chain('a'..'z').chain('0'..'9').collect_vec();
         if self.bricks.is_empty() {
             return "[no bricks]".into();
         }
         let max_x = self
             .bricks
             .iter()
-            .map(|brick| brick.1.x.max(brick.0.x))
+            .map(|(_, brick)| brick.1.x.max(brick.0.x))
             .max()
             .unwrap_or(0);
         let shape = GridShape {
@@ -140,21 +229,21 @@ impl World {
             let x = coord.x;
             // we want to render with 0 at the bottom
             let z = shape.height as isize - coord.y - 1;
-            let brick_indices = self
+            let brick_ids = self
                 .bricks
                 .iter()
-                .enumerate()
                 .filter(|(_, brick)| brick.x_range().contains(&x) && brick.z_range().contains(&z))
-                .map(|(i, _)| i)
+                .map(|(id, _)| id)
                 .collect_vec();
 
-            if brick_indices.len() > 1 {
+            if brick_ids.len() > 1 {
                 '?' // indicates multiple bricks
-            } else if brick_indices.len() == 1 {
-                if brick_indices[0] > 9 {
+            } else if brick_ids.len() == 1 {
+                let id_idx = *brick_ids[0];
+                if id_idx > visible_ids.len() {
                     '#' // indicates a brick with an index too large to render in a single char
                 } else {
-                    brick_indices[0].to_string().chars().next().unwrap()
+                    visible_ids[id_idx]
                 }
             } else {
                 '.'
@@ -169,7 +258,7 @@ impl World {
         let max_y = self
             .bricks
             .iter()
-            .map(|brick| brick.1.y.max(brick.0.y))
+            .map(|(_, brick)| brick.1.y.max(brick.0.y))
             .max()
             .unwrap_or(0);
         let shape = GridShape {
@@ -181,21 +270,17 @@ impl World {
             let y = coord.x;
             // we want to render with 0 at the bottom
             let z = shape.height as isize - coord.y - 1;
-            let brick_indices = self
+            let brick_ids = self
                 .bricks
                 .iter()
-                .enumerate()
                 .filter(|(_, brick)| brick.y_range().contains(&y) && brick.z_range().contains(&z))
-                .map(|(i, _)| i)
+                .map(|(id, _)| id)
                 .collect_vec();
-            if brick_indices.len() > 1 {
+
+            if brick_ids.len() > 1 {
                 '?' // indicates multiple bricks
-            } else if brick_indices.len() == 1 {
-                if brick_indices[0] > 9 {
-                    '#' // indicates a brick with an index too large to render in a single char
-                } else {
-                    brick_indices[0].to_string().chars().next().unwrap()
-                }
+            } else if brick_ids.len() == 1 {
+                friendlify_id(*brick_ids[0])
             } else {
                 '.'
             }
@@ -214,16 +299,20 @@ impl World {
     }
 }
 
+fn friendlify_id(id: usize) -> char {
+    let visible_ids = ('A'..'Z').chain('a'..'z').chain('0'..'9').collect_vec();
+    visible_ids.get(id).copied().unwrap_or('#')
+}
+
 impl FromStr for World {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        let mut world = World {
-            bricks: s
-                .lines()
+        let mut world = World::new(
+            s.lines()
                 .map(|line| line.parse())
                 .collect::<Result<Vec<_>>>()?,
-        };
+        );
         world.sort();
         Ok(world)
     }
@@ -235,9 +324,10 @@ mod test {
 
     #[test]
     fn test_part1() {
+        // not 1023
         assert_eq!(
             super::Day22.part1().unwrap().unwrap(),
-            "Hello, world!".to_string(),
+            "".to_string(),
         );
     }
 
@@ -261,5 +351,19 @@ mod test {
         assert_eq!(world.get_top_z(), Some(9));
         world.apply_gravity();
         assert_eq!(world.get_top_z(), Some(6));
+    }
+
+    #[test]
+    fn test_support() {
+        let mut world = sample_input();
+        world.apply_gravity();
+        assert_eq!(world.is_load_bearing(0), Some(true)); // A
+        assert_eq!(world.is_load_bearing(1), Some(false)); // B
+        assert_eq!(world.is_load_bearing(2), Some(false)); // C
+        assert_eq!(world.is_load_bearing(3), Some(false)); // D
+        assert_eq!(world.is_load_bearing(4), Some(false)); // E
+        assert_eq!(world.is_load_bearing(5), Some(true)); // F
+        assert_eq!(world.is_load_bearing(6), Some(false)); // G
+        assert_eq!(world.find_non_load_bearing_bricks(), 5);
     }
 }
